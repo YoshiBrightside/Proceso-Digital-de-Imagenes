0a6f786056b729aeaf2bbada6746c5bb5801e969
Joshua Pedrero <yoshibrightside@ciencias.unam.mx>
2021-08-16 23:08:38 -0500

    Practica 4 finished with modded filter class

--- Filtros/filters.py
+++ Filtros/filters.py
@@ -1,16 +1,6 @@
 import copy
 from PIL import Image, ImageFont, ImageDraw
 
-class StringVar():
-  '''
-  class StringVar just so we can skip using horrible tkinter's StringVar class
-  '''
-  def __init__(self, string):
-    self.string = string
-
-  def get(self):
-    return self.string
-
 class Filter:
   def __init__(self, name, description, options=None):
     self.name = name
@@ -18,55 +8,70 @@ class Filter:
     self.options = options
 
   def apply_filter(self, filter_name, image, options=None):
+    '''
+    Metodo que invoca el filtro seleccionado a partir de getattr. Una vez
+    finalizado el proceso, guarda una imagen temporal como '.temp.jpeg'
+    '''
     try:
       method = getattr(Filter, filter_name)
     except AttributeError:
       return image
     modded_image = method(self, image, options)
-    if modded_image:
-      print('saving...')
+    if modded_image != None:
       modded_image.save('.temp.jpeg')
 
-  def gray_tone(self, image, options={'filter_type':StringVar('RGB')}):
+  def gray_tone(self, image, options={'filter_type':'RGB'}):
+    '''
+    Filtro en escala de grises. Recibe la imagen a filtrar y la procesa a
+    partir de la escala de grises seleccionada en options. 
+    '''
     arr = image.load()
     for x in range(image.width):
       for y in range(image.height):
-        if options['filter_type'].get() == 'RGB':
+        if options['filter_type'] == 'RGB':
           grey = (arr[x, y][0] + arr[x, y][1] + arr[x, y][2])//3
-        if options['filter_type'].get() == 'R.3G.59B.11':
+        if options['filter_type'] == 'R.3G.59B.11':
           grey = int(arr[x, y][0]*.3) + int(arr[x, y][1]*.59) + int(arr[x, y][2]*.11)
-        if options['filter_type'].get() == 'R.21G.71B.07':
+        if options['filter_type'] == 'R.21G.71B.07':
           grey = int(arr[x, y][0]*.21) + int(arr[x, y][1]*.71) + int(arr[x, y][2]*.07)
-        if options['filter_type'].get() == 'RmaxGBmin':
+        if options['filter_type'] == 'RmaxGBmin':
           grey = (max(arr[x, y][0], arr[x, y][1], arr[x, y][2]) +
                   min(arr[x, y][0], arr[x, y][1], arr[x, y][2])) // 2
-        if options['filter_type'].get() == 'Max':
+        if options['filter_type'] == 'Max':
           grey = max(arr[x, y][0], arr[x, y][1], arr[x, y][2])
-        if options['filter_type'].get() == 'Min':
+        if options['filter_type'] == 'Min':
           grey = min(arr[x, y][0], arr[x, y][1], arr[x, y][2])
-        if options['filter_type'].get() == 'R':
+        if options['filter_type'] == 'R':
           grey = arr[x, y][0]
-        if options['filter_type'].get() == 'G':
+        if options['filter_type'] == 'G':
           grey = arr[x, y][1]
-        if options['filter_type'].get() == 'B':
+        if options['filter_type'] == 'B':
           grey = arr[x, y][2]
         arr[x, y] = (grey, grey, grey)
     return image
 
-  def brightness(self, image, options={'quantity':StringVar('10')}):
+  def brightness(self, image, options={'quantity':'10'}):
+    '''
+    Filtro para cambiar el brillo. Recibe la imagen a filtrar y la procesa a
+    partir del valor recibido en options.  
+    '''
     arr = image.load()
     for x in range(image.width):
       for y in range(image.height):
-        modifier = int(options['quantity'].get())
+        modifier = int(options['quantity'])
         arr[x, y] = (min(max(arr[x, y][0] + modifier, 0), 255),
                      min(max(arr[x, y][1] + modifier, 0), 255),
                      min(max(arr[x, y][2] + modifier, 0), 255))
     return image
 
-  def mosaic(self, image, options={'size':StringVar('10')}):
+  def mosaic(self, image, options={'size':'10'}):
+    '''
+    Filtro para "pixelar" una imagen. Recibe la imagen a pixelar y la procesa a
+    partir de tamanio del mosaico recibido en options.
+    '''
     arr = image.load()
     w, h = image.width, image.height
-    size = int(options['size'].get())
+    size = int(options['size'])
     x, y, r, g, b, m, n = 0, 0, 0, 0, 0, 0, 0
     while x < w:
       m = size if (x + size) < w else w-x
@@ -86,11 +91,19 @@ class Filter:
     return image
 
   def high_contrast(self, image, options=None):
+    '''
+    Ver metodo inverse().
+    (-) (-) = +
+    '''
     self.inverse(image)
     self.inverse(image)
     return image
 
   def inverse(self, image, options=None):
+    '''
+    Revisa cada pixel y lo define como negro o blanco, segun los valores rgb
+    que tengan. Recibe una imagen.
+    '''
     arr = image.load()
     for x in range(image.width):
       for y in range(image.height):
@@ -100,19 +113,27 @@ class Filter:
           arr[x, y] = (255, 255, 255)
     return image
 
-  def rgb_filter(self, image, options={'channel':StringVar('Red')}):
+  def rgb_filter(self, image, options={'channel':'Red'}):
+    '''
+    Elimina los valores que no sean del canal seleccionado.
+    '''
     arr = image.load()
     for x in range(image.width):
       for y in range(image.height):
-        if options['channel'].get() == 'Red':
+        if options['channel'] == 'Red':
           arr[x, y] = (arr[x, y][0], 0, 0)
-        if options['channel'].get() == 'Green':
+        if options['channel'] == 'Green':
           arr[x, y] = (0, arr[x, y][1], 0)
-        if options['channel'].get() == 'Blue':
+        if options['channel'] == 'Blue':
           arr[x, y] = (0, 0, arr[x, y][2])
     return image
 
-  def convolusion(self, image, matrix, factor= 1, bias=0):
+  def convolusion(self, image, matrix, factor=1, bias=0):
+    '''
+    Toma una matriz especifica a un filtro y la aplica a una imagen.
+    Recibe una imagen, un factor en caso de necesitar normalizar, y un valor
+    para compensar los valores rgb de un pixel.
+    '''
     img_copy = copy.deepcopy(image)
     arr = image.load()
     copy_arr = img_copy.load()
@@ -138,8 +159,11 @@ class Filter:
     return image
     
 
-  def blur(self, image, options={'size':StringVar('3x3')}):
-    size = get_matrix_size(options['size'].get())
+  def blur(self, image, options={'size':'3x3'}):
+    '''
+    Filtro para desenfocar imagenes. Recibe una imagen y un valor de matriz.
+    '''
+    size = get_matrix_size(options['size'])
     if size == 3:
       matrix =[[0, .2, 0],
               [.2, .2, .2],
@@ -154,7 +178,11 @@ class Filter:
       factor = 1/13
     self.convolusion(image, matrix, factor=factor)
 
-  def motion_blur(self, image, options={'size':StringVar('9x9')}):
+  def motion_blur(self, image, options={'size':'9x9'}):
+    '''
+    Filtro para desenfocar imagenes con cierta direccion de movimiento.
+    Recibe una imagen y un valor de matriz.
+    '''
     matrix =[[1, 0, 0, 0, 0, 0, 0, 0, 0],
              [0, 1, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 1, 0, 0, 0, 0, 0, 0],
@@ -166,7 +194,10 @@ class Filter:
              [0, 0, 0, 0, 0, 0, 0, 0, 1]]
     self.convolusion(image, matrix, factor=(1/9))
 
-  def find_edges(self, image, options={'size':StringVar('5x5')}):
+  def find_edges(self, image, options={'size':'5x5'}):
+    '''
+    Filtro para encontrar orillas. Recibe una imagen y un valor de matriz.
+    '''
     matrix =[[-1, 0, 0, 0, 0],
              [ 0, -2, 0, 0, 0],
              [ 0, 0, 6, 0, 0],
@@ -174,13 +205,19 @@ class Filter:
              [ 0, 0, 0, 0, -1]]
     self.convolusion(image, matrix, factor=(1/5))
 
-  def sharpen(self, image, options={'size':StringVar('3x3')}):
+  def sharpen(self, image, options={'size':'3x3'}):
+    '''
+    Filtro para 'afilar' imagenes. Recibe una imagen y un valor de matriz.
+    '''
     matrix =[[-1, -1, -1],
              [-1, 9, -1],
              [-1, -1, -1]]
     self.convolusion(image, matrix)
 
-  def emboss(self, image, options={'size':StringVar('5x5')}):
+  def emboss(self, image, options={'size':'5x5'}):
+    '''
+    Filtro para 'abollar' imagenes. Recibe una imagen y un valor de matriz.
+    '''
     matrix =[[-1, -1, -1, -1, 0],
              [-1, -1, -1, 0, 1],
              [-1, -1, 0, 1, 1],
@@ -188,23 +225,30 @@ class Filter:
              [ 0, 1, 1, 1, 1]]
     self.convolusion(image, matrix, bias=128)
 
-  def watermark(self,
+  def marca_de_agua(self,
                 image,
-                options={'text':StringVar('text'),
-                         'font_size':StringVar('20'),
-                         'transparency':StringVar('130')}):
-    # print(options['text'].get(), options['font_size'].get(), options['transparency'].get())
+                options={'texto':'text',
+                         'tamano':'20',
+                         'transparencia':'130'}):
+    '''
+    Filtro marca de agua. Crea una imagen con el texto deseado y la compone
+    con la imagen recibida para obtener la imagen.
+    '''
     txt_img = Image.new('RGBA', image.size, (255,255,255,0))
     draw = ImageDraw.Draw(txt_img)
-    text = 'This filter is on demo :)'#options['text'].get()
-    transparency = 120 #int(options['transparency'].get())
-    font = ImageFont.truetype("Gidole-Regular.ttf",  30) #int(options['font_size'].get()))
-    draw.text((10,10), text, font=font, fill=(255,255,255, transparency))
+    text = options['texto']
+    x, y = int(options['x']), int(options['y'])
+    transparency = int(options['transparencia'])
+    font = ImageFont.truetype("Gidole-Regular.ttf", int(options['tamano']))
+    draw.text((x,y), text, font=font, fill=(255,255,255, transparency))
     image = Image.alpha_composite(image.convert('RGBA'), txt_img)
     image = image.convert('RGB')
     return image
 
 def get_matrix_size(s):
+  '''
+  Recibe un string s con formato NxN, y devuelve el valor N en entero.
+  '''
   return int(s.split('x')[0])
 
 def get_filters():
@@ -257,11 +301,17 @@ def get_filters():
                         'test',
                         {'size':{'style':'dropdown',
                                  'values':['5x5']}}))
-  filters.append(Filter('watermark',
+  filters.append(Filter('marca_de_agua',
                         'test',
-                        {'text':{'style':'text_box'},
-                        'font_size':{'style':'text_box'},
-                        'transparency':{'style':'text_box'}}))
+                        {'texto':{'style':'text_box'},
+                        'tamano':{'style':'text_box'},
+                        'transparencia':{'style':'text_box'},
+                        'x':{'style':'text_box'},
+                        'y':{'style':'text_box'}}))
+  filters.append(Filter('recursive',
+                        'test_description',
+                        {'size':{'style':'dropdown',
+                                 'values':['10x10','20x20','30x30']}}))
   return filters
 
 if __name__ == '__main__':
